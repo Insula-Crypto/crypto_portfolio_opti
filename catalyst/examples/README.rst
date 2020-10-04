f701c0f45578c10e6aa0cab158d4efca6d63f6a0
emilecohen <emile.cohen@dathena.io>
2020-10-04 21:26:48 +0200

    new code

--- catalyst/examples/README.rst
+++
@@ -1,3 +0,0 @@
-An overview of most of the trading strategies in this folder can be found in the 
-`Examples Algorithms <https://enigmampc.github.io/catalyst/example-algos.html>`_
-section of our documentation website.
--- catalyst/support/ccxt_issue_1358.py
+++
@@ -1,8 +0,0 @@
-import ccxt
-
-bitfinex = ccxt.bitfinex()
-bitfinex.verbose = True
-ohlcvs = bitfinex.fetch_ohlcv('ETH/BTC', '30m', 1504224000000)
-
-dt = bitfinex.iso8601(ohlcvs[0][0])
-print(dt)  # should print '2017-09-01T00:00:00.000Z'
